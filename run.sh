99307d6a3f1d7be64d594b8c8cff0a75e41c3723
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2023-07-27 19:15:23 +0200

    Init

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# /proc/1/fd/1 is systemd's stdout, and therefore appears in the resin logs.
+pushd /usr/src/app
+pwd >/proc/1/fd/1
+ls -lh >/proc/1/fd/1
+python3.10 push_temps.py  >/proc/1/fd/1
+popd
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# /proc/1/fd/1 is systemd's stdout, and therefore appears in the resin logs.
+fcrontab -l &>/proc/1/fd/1
+fcrontab -l &>1
+fcron -f -d &>/proc/1/fd/1
