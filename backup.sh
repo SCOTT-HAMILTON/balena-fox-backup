1ac5aeb93a4eabbf33102e3028fe70eaa5c8560d
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2023-08-01 17:30:00 +0200

    restic backup

--- backup.sh
+++ backup.sh
@@ -6,13 +6,11 @@ pwd &>/proc/1/fd/1
 export AWS_SECRET_ACCESS_KEY=$(jq -r '.["s3"].secretAccessKey' auth.json)
 export AWS_ACCESS_KEY_ID=$(jq -r '.["s3"].accessKeyId' auth.json)
 export S3_ENDPOINT=$(jq -r '.["s3"].endpoint' auth.json)
-echo $RESTIC_S3_REPO_PASSWORD>passwd
 
 python3.10 download_temps.py &>/proc/1/fd/1
-restic snapshots -r s3:$S3_ENDPOINT/temps-backup -p passwd &>/proc/1/fd/1
+restic snapshots -r s3:$S3_ENDPOINT/temps-backup -p /tmp/restic_pass &>/proc/1/fd/1
 ls -lh downloads_backup &>/proc/1/fd/1
-RESTIC_COMPRESSION=max restic backup -r s3:$S3_ENDPOINT/temps-backup -p passwd downloads_backup/* &>/proc/1/fd/1
-restic prune -r s3:$S3_ENDPOINT/temps-backup -p passwd &>/proc/1/fd/1
+RESTIC_COMPRESSION=max restic backup -r s3:$S3_ENDPOINT/temps-backup downloads_backup/* -p /tmp/restic_pass &>/proc/1/fd/1
+restic prune -r s3:$S3_ENDPOINT/temps-backup -p /tmp/restic_pass &>/proc/1/fd/1
 
-rm passwd
 popd
--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # /proc/1/fd/1 is systemd's stdout, and therefore appears in the resin logs.
+echo $RESTIC_S3_REPO_PASSWORD>/tmp/restic_pass
 fcrontab -l &>/proc/1/fd/1
 fcrontab -l &>1
 fcron -f -d &>/proc/1/fd/1
